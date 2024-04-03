fbbdd3925c3717d2b3d696d3629e604f8819ec60
shubham <shubhamgags@gmail.com>
2024-04-03 16:22:13 +0530

    refactor app.js

--- src/App.js
+++ src/App.js
@@ -1,85 +1,42 @@
-import "./App.css";
-import {
-  createBrowserRouter,
-  Navigate,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
+// import react and routers
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import pages
 import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { Profile } from "./pages/profile/Profile";
 import { Register } from "./pages/register/Register";
-import { Navbar } from "./components/navbar/Navbar";
-import { LeftBar } from "./components/leftBar/LeftBar";
-import { Center } from "./components/center/Center";
-import { RightBar } from "./components/rightBar/RightBar";
+// import css
+import "./App.css";
 
+
+// Assume currentUser is obtained from some authentication context
+const currentUser = true;
+
+
+// ProtectedRoute component to handle authentication
+const ProtectedRoute = ({ route }) => {
+  // if current user is null redirect to login 
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+  // else redirect to the original route
+  return route;
+};
+
+
+// App component
 function App() {
-  const currentUser = true;
-
-  const Layout = () => {
-    return (
-      <div>
-        <Navbar />
-        <div className="main-container">
-          <LeftBar />
-          <Center />
-          <RightBar />
-        </div>
-      </div>
-    );
-  };
-
-  const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      return <Navigate to="/login/" />;
-    }
-    return children;
-  };
-  const router = createBrowserRouter([
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/navbar",
-      element: <Navbar />,
-    },
-    {
-      path: "/leftbar",
-      element: <LeftBar />,
-    },
-
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/profile/:id",
-          element: <Profile />,
-        },
-      ],
-    },
-  ]);
-
   return (
     <div className="App">
-      {/* <Login /> */}
-      <RouterProvider router={router} />
-      {/* <Home />
-      <Profile /> */}
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/profile/:id" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
