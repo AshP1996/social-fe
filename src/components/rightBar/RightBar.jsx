caae04c756b9851a8e3cb57348d132d33ecd51c8
Ashish <ashish01prajapati01@gmail.com>
2024-04-04 19:11:52 +0530

    new files

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,7 +1,49 @@
-// import "./rightBar.scss"
+import "./rightBar.scss"
+
+import { MdGroups as GroupIcon } from "react-icons/md";
+import { FaSitemap as FriendsIcon } from "react-icons/fa";
+import { MdOutlineWatchLater as WatchLaterIcon } from "react-icons/md";
+import { RiMemoriesFill as MemoriesIcon } from "react-icons/ri";
 
 export const RightBar = () => {
   return (
-    <div>RightBar</div>
+    <div className="rightBar">
+      <div className="upperRight">
+        <div className="upperItems">
+            <FriendsIcon />
+            <span>Friends</span>
+          </div>
+          <div className="upperItems">
+            <GroupIcon />   
+            <span>Groups</span>       
+          </div>
+          <div className="upperItems">
+            <WatchLaterIcon />
+            <span>Watch</span>
+          </div>
+          <div className="upperItems">
+            <MemoriesIcon />
+            <span>Memories</span>
+          </div>
+          <div className="upperItems">
+            <MemoriesIcon />
+            <span>Memories</span>
+          </div>
+      </div>
+      <div className="lowerRight">
+        <div className="lowerItems">
+            <MemoriesIcon />
+            <span>Memories</span>
+          </div>
+          <div className="lowerItems">
+            <MemoriesIcon />
+            <span>Memories</span>
+          </div>
+          <div className="lowerItems">
+            <MemoriesIcon />
+            <span>Memories</span>
+          </div>
+      </div>
+    </div>
   )
 }
